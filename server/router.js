ded9096f1f2780f74b3149fa2b08f498a6f50b28
ghost <454201948@qq.com>
2019-12-28 21:20:13 +0800

    chg: check token of api request

--- server/router.js
+++ server/router.js
@@ -17,35 +17,48 @@ router.get('/api/gulp', async (ctx, next) => {
 })
 
 router
+  .get('/api/user/admin',
+  user.getAdmin)
   .post('/api/user/login',
   user.login)
   .get('/api/user/logout',
+  check.auth('token'),
   user.logout)
 
 router
-  .use(check.auth('token'))
   .get('/api/category/all',
+  check.filter('token'),
   category.getCategories)
   .get('/api/category',
   category.getCategory)
   .post('/api/category',
+  check.auth('token'),
   category.newCategory)
   .delete('/api/category',
+  check.auth('token'),
   category.deleteCategory)
   .patch('/api/category',
+  check.auth('token'),
   category.patchCategory)
 
 router
-  // .use(check.auth('token'))
   .get('/api/articles',
+  check.filter('token'),
   article.getArticles)
   .get('/api/article',
+  check.filter('token'),
   article.getArticle)
+  .get('/api/articles-top',
+  check.filter('token'),
+  article.getArticleTop)
   .post('/api/article',
+  check.auth('token'),
   article.newArticle)
   .delete('/api/article',
+  check.auth('token'),
   article.deleteArticle)
   .patch('/api/article',
+  check.auth('token'),
   article.patchArticle)
-  
+
 module.exports = router
