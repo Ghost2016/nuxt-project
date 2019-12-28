d20372b5d0b615205b3f7a17fc649ac2eaa4d1a2
ghost <454201948@qq.com>
2019-12-28 21:19:15 +0800

    chg: add filter to patch userInfo when token exist

--- server/middlewares/check.middleware.js
+++ server/middlewares/check.middleware.js
@@ -9,7 +9,25 @@ exports.auth = name => async (ctx, next) => {
     
     await next()
   } catch (error) {
-    ctx.clearCookie(name)
+    // ctx.clearCookie(name)
     ctx.handleError('Token 无效', error)
   }
+}
+
+// 针对token非必须的get接口，如果token无效只返回部分信息
+exports.filter = name => async (ctx, next) => {
+  const t = ctx.cookies.get(name)
+  console.log(name, ': ' +t)
+  ctx.state.user = {}
+
+  if (t) {
+    try {
+      const userInfo = token.verify(t)
+      ctx.state.user = userInfo
+    } catch (error) {
+      console.error(error)
+      ctx.state.user = {}
+    }
+  }
+  await next()
 }
